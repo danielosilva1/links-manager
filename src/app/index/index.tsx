c98f4ecbe45dac201a643e479c51caab88fffa9c
vitor <danielosilva@alu.ufc.br>
2025-09-19 12:35:41 -0300

    feat: add modal to display link information

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,16 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 import { colors } from "@/styles/colors";
 import { styles } from "./styles";
 
@@ -33,6 +41,32 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkLabel}>Rocketseat</Text>
+            <Text style={styles.modalUrl}>https://rocketseat.com.br</Text>
+
+            <View style={styles.modalFooter}>
+              <Option label={"Excluir"} icon="delete" variant="secondary" />
+              <Option label={"Abrir"} icon="language" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
